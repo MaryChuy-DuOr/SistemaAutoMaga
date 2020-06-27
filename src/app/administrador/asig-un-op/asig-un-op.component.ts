8eec0b88dc0566936e94c39955c7c1406f0c06c8
Mary Chuy <maria.0299.ornelas@gmail.com>
2020-06-26 20:20:07 -0500

    Rol administrador

---
+++ src/app/administrador/asig-un-op/asig-un-op.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asig-un-op',
+  templateUrl: './asig-un-op.component.html',
+  styles: []
+})
+export class AsigUnOpComponent implements OnInit {
+
+  bandera1: boolean = false;
+  bandera2: boolean = false;
+  bandera3: boolean = false;
+  bandera4: boolean = false;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  mostrarTabla() {
+    if (this.bandera1) {
+      this.bandera1 = false;
+    } else {
+      this.bandera1 = true;
+    }
+  }
+
+  registrar() {
+    if (this.bandera2) {
+      this.bandera2 = false;
+    } else {
+      this.bandera2 = true;
+    }
+  }
+
+  alert1() {
+    if (this.bandera3) {
+      this.bandera3 = false;
+    } else {
+      this.bandera3 = true;
+    }
+  }
+  alert2() {
+    if (this.bandera4) {
+      this.bandera4 = false;
+    } else {
+      this.bandera4 = true;
+    }
+  }
+
+}
