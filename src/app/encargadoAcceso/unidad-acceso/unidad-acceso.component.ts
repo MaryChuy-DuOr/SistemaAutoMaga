6865dd76b83245ed4e8501d9eef771bc9501fd7c
Mary Chuy <maria.0299.ornelas@gmail.com>
2020-06-27 13:08:28 -0500

    Encargado de acceso terminado

---
+++ src/app/encargadoAcceso/unidad-acceso/unidad-acceso.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unidad-acceso',
+  templateUrl: './unidad-acceso.component.html',
+  styles: []
+})
+export class UnidadAccesoComponent implements OnInit {
+
+  bandera1: boolean = false;
+  bandera2: boolean = false;
+  bandera3: boolean = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  mostrarTabla() {
+    if (this.bandera1) {
+      this.bandera1 = false;
+    } else {
+      this.bandera1 = true;
+    }
+  }
+
+  registrar() {
+    if (this.bandera2) {
+      this.bandera2 = false;
+    } else {
+      this.bandera2 = true;
+    }
+  }
+
+  imprimir() {
+    if (this.bandera3) {
+      this.bandera3 = false;
+    } else {
+      this.bandera3 = true;
+    }
+  }
+
+}
+
